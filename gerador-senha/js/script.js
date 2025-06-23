d186adca0a29ab62bb8f9dbeeda2c3dbcea32665
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-23 14:49:58 +0100

    Script que vai gerar a senha

---
+++ gerador-senha/js/script.js
@@ -0,0 +1,9 @@
+let form = document.getElementById("form")
+let senha = document.getElementById("senha")
+let copy = document.querySelector(".fas")
+
+//função que gera a senha
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	alert("Estou funcionando")
+})
