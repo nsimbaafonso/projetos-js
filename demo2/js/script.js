4ef7c7e12dc52ace0936c9143c40c58ece83d1fb
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-23 13:49:28 +0100

    novo padrão de projeto JS criado

---
+++ demo2/js/script.js
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById("search-form") //o formulário
+const searchBox = document.getElementById("search-box") //o input
+const searchResult = document.getElementById("search-result") //a div que mostra as imagens
+const mostrarMais = document.getElementById("mostrar-mais") //o botão que exibe mais imagens
+
